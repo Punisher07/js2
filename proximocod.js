de6a511c57c53fd223e4f708d126eb187fe439ff
Punisher07 <Punisher07@users.noreply.github.com>
2017-09-29 16:27:38 -0300

    Com onload

--- proximocod.js
+++ proximocod.js
@@ -1,19 +1,26 @@
 // Rotina Javascript para arrumar "próximo código" do cadastro de roupas.
 // Desenvolvido (de uma maneira MUITO simples) por Paulo César Fratantonio
 
-window.localStorage.setItem('pcod', 3000000);	// # # USAR SOMENTE A 1A VEZ! # #
-var pcodigo = window.localStorage.getItem('pcod'); // TRAZ O CODIGO DO STORAGE
-document.getElementById("id_sc_field_codigo").value = pcodigo; // ACERTA O CAMPO DO CÓDIGO
-
-sc_b_ins_t.onclick = function inccod(); // adiciona função no incluir que incrementa contagem
-
-function inccod()
+<script type="text/javascript">
+window.onload = function()
 {
-	var codigo = document.getElementById("id_sc_field_codigo").value;
-	if (codigo > 2000000)
+
+	window.localStorage.setItem('pcod', 3000000);	// # # USAR SOMENTE A 1A VEZ! # #
+	var pcodigo = window.localStorage.getItem('pcod'); // TRAZ O CODIGO DO STORAGE
+	document.getElementById("id_sc_field_codigo").value = pcodigo; // ACERTA O CAMPO DO CÓDIGO
+
+	sc_b_ins_t.onclick = function inccod(); // adiciona função no incluir que incrementa contagem
+
+	function inccod()
 	{
-		{ pcodigo++;window.localStorage.setItem('pcod', pcodigo);
+		var codigo = document.getElementById("id_sc_field_codigo").value;
+		if (codigo > 2000000)
+		{
+			{ pcodigo++;window.localStorage.setItem('pcod', pcodigo);
+		}
 	}
-}
+
+</script>
+
 
 // javascript:(function () { $('<script>').attr('src', 'https://raw.githubusercontent.com/Punisher07/js2/master/proximocod.js'); })();
