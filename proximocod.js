d6d383b40f404c705ad73a1656685f3b97474c6b
Punisher07 <Punisher07@users.noreply.github.com>
2017-10-05 17:19:42 -0300

    Add files via upload

--- proximocod.js
+++ proximocod.js
@@ -1,16 +1,12 @@
-// Rotina Javascript para arrumar "próximo código" do cadastro de roupas.
-// Desenvolvido (de uma maneira MUITO simples) por Paulo César Fratantonio
 function inccod()
 {
-	var codigo = document.getElementById("id_sc_field_codigo").value;
-	if (codigo > 2000000)
-	{
+		alert(pcodigo);
 		pcodigo++;
 		window.localStorage.setItem('pcod', pcodigo);
-	}
+		alert(pcodigo);
+		document.getElementById('id_sc_field_codigo').value = pcodigo;
 };
 window.localStorage.setItem('pcod', 3500001);
 var pcodigo = window.localStorage.getItem('pcod');
-document.getElementById("id_sc_field_codigo").value = pcodigo;
-document.getElementById("sc_b_sai_t").onclick = inccod();
-// javascript:(function () { $('<script>').attr('src', 'https://raw.githubusercontent.com/Punisher07/js2/master/proximocod.js'); })();
+document.getElementById('id_sc_field_codigo').value = pcodigo;
+document.getElementById("id_sc_field_descricao").onChange = inccod();
